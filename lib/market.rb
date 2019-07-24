0f9908684dc8d1285feecbb3e89d02f4d2e0629b
Tylor Schafer <tylorschafer@gmail.com>
2019-07-24 10:46:19 -0600

    Add sorted_item and total_inventory to Vendor class

--- lib/market.rb
+++ lib/market.rb
@@ -18,4 +18,17 @@ class Market
     @vendors.find_all {|vendor| vendor.check_stock(item) != 0}
   end
 
+  def sorted_item_list
+    @vendors.map {|vendor| vendor.inventory.keys}.flatten.uniq.sort
+  end
+
+  def total_inventory
+    total_inventory = Hash.new(0)
+    @vendors.each do |vendor|
+      vendor.inventory.each do |item, quantity|
+        total_inventory[item] += quantity
+      end
+    end
+    total_inventory
+  end
 end
